a87de8c0c9802470b09e642a038fd5f382f36770
ALWIN JOHNS <alwinjohns@icloud.com>
2018-03-21 23:24:54 -0500

    basic framework

---
+++ resolvers.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose'
+import projectModel from './models/project.js'
+import bidModel from './models/bid.js'
+
+const resolvers = {
+    Query: {
+        projects: () => {
+            return projectModel.find()
+        },
+        project: (root, { id }) => {
+            return projectModel.findOne({id})
+        },
+        bids: (root, { projectId }) => {
+            return bidModel.find({projectId})
+        },
+        bid: (root, { projectId, bidId }) => {
+            return bidModel.findOne({id: bidId, projectId})
+        }
+    },
+    Mutation: {
+        addProject: (root, {projectName, sellerId, description, bidStart, bidEnd, maxAmount}) => {
+            const project = new projectModel({projectName, sellerId, description, bidStart, bidEnd, maxAmount, lowestBid: null})
+            return project.save()
+        },
+        deleteProject: (root, { projectId }) => {
+            return projectModel.findOneAndRemove({id: projectId})
+        },
+        updateProject: (root, { projectId, projectName, description }) => {
+            return projectModel.findOneAndUpdate({id: projectId}, {projectName, description})
+        },
+        addBid: (root, {projectId, buyerId, bidAmount}) => {
+            const newBid = new bidModel({projectId, buyerId, bidAmount})
+            return newBid.save()
+        },
+        updateBid: (root, { bidId, bidAmount }) => {
+            return bidModel.findOneAndUpdate({id: bidId}, {bidAmount})
+        },
+        deleteBid: (root, { bidId }) => {
+            return bidModel.findOneAndRemove({id: bidId})
+        }
+    }
+}
+export default resolvers
---
+++ schema.js
@@ -0,0 +1,48 @@
+import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools'
+import resolvers from './resolvers'
+
+const typeDefs = `
+type LowBid {
+    buyerId: String,
+    bidAmount: Int,
+    bidId: String,
+}
+type Bid {
+    buyerId: String,
+    bidAmount: Int,
+    id: String,
+    projectId: String,
+}
+type Project {
+    id: String,
+    projectName: String,
+    sellerId: String,
+    description: String,
+    bidStart: String,
+    bidEnd: String,
+    maxAmount: Int,
+    lowestBid: LowBid,
+}
+type Query {
+    projects: [Project],
+    project(id: String): Project
+    bids(projectId: String): [Bid]
+    bid(projectId: String, bidId: String): Bid
+}
+type Mutation {
+    addProject(projectName: String!, sellerId: String!, description: String, bidStart: String, bidEnd: String!, maxAmount: Int!): Project,
+    deleteProject(projectId: String!): Project,
+    updateProject(projectId: String!, projectName: String!): Project,
+    addBid(projectId: String!, buyerId: String!, bidAmount: Int!): Bid,
+    updateBid(bidId: String!, bidAmount: Int!): Bid,
+    deleteBid(bidId: String!): Bid
+}
+`
+
+const schema = makeExecutableSchema({
+    typeDefs: [typeDefs],
+    resolvers
+})
+// addMockFunctionsToSchema({schema})
+
+export default schema
